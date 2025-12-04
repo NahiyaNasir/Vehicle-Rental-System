0411c2e3da4a0ffc84d6c72214b0bba61a68f1a8
NahiyaNasir <nahiaesha.01@gmail.com>
2025-12-04 22:20:27 +0600

    config folder created

---
+++ src/config/Db.ts
@@ -0,0 +1,22 @@
+import config from ".";
+import { Pool } from "pg";
+
+export const pool = new Pool({
+  connectionString: `${config.connection_str}`,
+});
+ console.log(config.connection_str);
+const initDB = async () => {
+  await pool.query(`
+   CREATE TABLE IF NOT EXISTS users(
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(100) NOT NULL,
+        email VARCHAR(150) NOT NULL UNIQUE CHECK (email = LOWER(email)),
+        password  VARCHAR(100) NOT NULL  CHECK (LENGTH(password) >= 6),
+        role VARCHAR(50) CHECK (role IN ('admin', 'customer')),
+        phone VARCHAR(15) NOT NULL ,
+        created_at TIMESTAMP DEFAULT NOW(),
+        updated_at TIMESTAMP DEFAULT NOW()
+        )
+  `);
+};
+export default      initDB();
--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,10 @@
+import config from "./config";
+import express  from "express";
+const port = config.port;
+const app = express();
+// parser
+app.use(express.json());
 
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
